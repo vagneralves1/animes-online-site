32d6c3829586cbdf54c8c56389b9088fdacec343
vagneralves1 <vagner.alves.js@gmail.com>
2024-01-07 22:45:48 -0300

    Salve animes online

---
+++ java_anime.js
@@ -0,0 +1,65 @@
+var estrela = document.getElementById('estrela_cor')
+var estrela2 = document.getElementById('estrela_cor2')
+var estrela3 = document.getElementById('estrela_cor3')
+var estrela4 = document.getElementById('estrela_cor4')
+var estrela5 = document.getElementById('estrela_cor5')
+
+function entrou(){
+    estrela.style.background = 'rgb(200, 19, 19)'
+}
+
+function movendo(){
+    estrela.style.background = 'rgb(200, 19, 19)'
+}
+
+function saiu(){
+    estrela.style.background = 'rgb(255, 225, 0)'
+}
+
+function entrou2(){
+    estrela2.style.background = 'rgb(200, 19, 19)'
+}
+
+function movendo2(){
+    estrela2.style.background = 'rgb(200, 19, 19)'
+}
+
+function saiu2(){
+    estrela2.style.background = 'rgb(255, 225, 0)'
+}
+
+function entrou3(){
+    estrela3.style.background = 'rgb(200, 19, 19)'
+}
+
+function movendo3(){
+    estrela3.style.background = 'rgb(200, 19, 19)'
+}
+
+function saiu3(){
+    estrela3.style.background = 'rgb(255, 225, 0)'
+}
+
+function entrou4(){
+    estrela4.style.background = 'rgb(200, 19, 19)'
+}
+
+function movendo4(){
+    estrela4.style.background = 'rgb(200, 19, 19)'
+}
+
+function saiu4(){
+    estrela4.style.background = 'rgb(255, 225, 0)'
+}
+
+function entrou5(){
+    estrela5.style.background = 'rgb(200, 19, 19)'
+}
+
+function movendo5(){
+    estrela5.style.background = 'rgb(200, 19, 19)'
+}
+
+function saiu5(){
+    estrela5.style.background = 'rgb(255, 225, 0)'
+}
